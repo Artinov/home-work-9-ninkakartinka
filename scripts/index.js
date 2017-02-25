9d9b28c81b0ea021e1f7f893f2845df917be00fc
ninkakartinka <ninkakartinka98@gmail.com>
2017-02-25 14:48:30 +0200

    Nothing

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,11 @@
 var ball = document.querySelector(".discoball");
+var classAll = ["here","goes", "our", "awesome", "disco", "ball"];
 
+ 
 function goDisco() {
-	//do your disco magic here !
+
+ball.classList.add(classAll[0,1,2,3,4,5]);
+
 }
 
 setInterval(goDisco,300);
