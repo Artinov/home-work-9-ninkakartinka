8618b89dfe13e728e261c1f3141cf620ea995dc7
ninkakartinka <ninkakartinka98@gmail.com>
2017-02-25 16:23:15 +0200

    Done

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,15 @@
 var ball = document.querySelector(".discoball");
 var classAll = ["here","goes", "our", "awesome", "disco", "ball"];
-
+var i=0;
  
 function goDisco() {
+	ball.setAttribute("class", "discoball " + classAll[i]);
+	i++;
+	console.log(classAll[i]);
+	if (i>=5){
+		i=0;
+	}
 
-ball.classList.add(classAll[0,1,2,3,4,5]);
 
 }
 
